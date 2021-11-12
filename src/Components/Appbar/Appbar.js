31f5d9d68457e9ea0be277a1a699516b6d405d2f
taha26 <tahakhan26402@gmail.com>
2021-11-12 17:17:55 +0500

    App Created And Header, NavBar Implemented

---
+++ src/Components/Appbar/Appbar.js
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import PhoneAndroidOutlinedIcon from '@mui/icons-material/PhoneAndroidOutlined';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import TwitterIcon from '@mui/icons-material/Twitter';
+// import Divider from '@mui/material/Divider';
+// import Typography from '@mui/material/Typography';
+// import Button from '@mui/material/Button';
+// import IconButton from '@mui/material/IconButton';
+// import MenuIcon from '@mui/icons-material/Menu';
+
+import './Appbar.css'
+
+
+export const Appbar = () => {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar style={{ backgroundColor: "#7EC23E" }} position="static">
+                {/* <Toolbar > */}
+                <div className="appbar-div">
+
+                    <div className="appbar-div-1">
+
+                        {/* <div> */}
+                        <EmailOutlinedIcon />
+                        <span>laserworldaesthetic@gmail.com </span>
+                        <PhoneAndroidOutlinedIcon />
+                        <span>0300-9378263 </span>
+                        {/* </div> */}
+                    </div>
+                    <div className="appbar-div-2">
+                        <FacebookIcon className="appbar-div-2-icons" />
+                        <WhatsAppIcon className="appbar-div-2-icons" />
+                        <TwitterIcon className="appbar-div-2-icons" />
+                        {/* <Divider orientation="vertical" variant="middle" flexItem /> */}
+                        <span>
+
+                            {/* <h4> */}
+                            FAQs
+                            {/* </h4> */}
+                        </span>
+                    </div>
+                </div>
+                {/* <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton> */}
+                {/* <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        News
+                    </Typography> */}
+                {/* <Button color="inherit">Login</Button> */}
+                {/* </Toolbar> */}
+            </AppBar>
+        </Box>
+    );
+}
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,65 @@
+import * as React from 'react';
+// import AppBar from '@mui/material/AppBar';
+// import Box from '@mui/material/Box';
+// import Toolbar from '@mui/material/Toolbar';
+// import Typography from '@mui/material/Typography';
+// import Button from '@mui/material/Button';
+// import IconButton from '@mui/material/IconButton';
+// import MenuIcon from '@mui/icons-material/Menu';
+import './Header.css';
+
+
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from 'react-bootstrap/Container';
+import NavDropdown from "react-bootstrap/NavDropdown"
+
+import Logo from "../../Assets/logo.png"
+
+export const Header = () => {
+    return (
+        // <Box sx={{ flexGrow: 1 }}>
+        //     <AppBar position="static">
+        //         <Toolbar>
+        //             <IconButton
+        //                 size="large"
+        //                 edge="start"
+        //                 color="inherit"
+        //                 aria-label="menu"
+        //                 sx={{ mr: 2 }}
+        //             >
+        //                 <MenuIcon />
+        //             </IconButton>
+        //             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        //                 News
+        //             </Typography>
+        //             <Button color="inherit">Login</Button>
+        //         </Toolbar>
+        //     </AppBar>
+        // </Box>
+        <Navbar className="header-navbar" bg="light" expand="lg">
+            {/* <Container className="header-container"> */}
+            <Navbar.Brand className="header-container-img" href="#home">
+                <img alt="logo" width="100%" src={Logo} />
+            </Navbar.Brand>
+            <Navbar.Toggle className="abc" aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link className="header-link" href="#home">Home</Nav.Link>
+                    <Nav.Link className="header-link" href="#link">About</Nav.Link>
+                    <NavDropdown className="header-link" title="Services" id="basic-nav-dropdown">
+                        <NavDropdown.Item className="header-dropdown" href="#action/3.1">FUE</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item className="header-dropdown" href="#action/3.2">PRP</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item className="header-dropdown" href="#action/3.3">Dermatology & Transmotrology</NavDropdown.Item>
+                        {/* <NavDropdown.Item className="header-dropdown" href="#action/3.4">Separated link</NavDropdown.Item> */}
+                    </NavDropdown>
+                    <Nav.Link className="header-link" href="#link">Appointment</Nav.Link>
+                    <Nav.Link className="header-link" href="#link">Contact us</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+            {/* </Container> */}
+        </Navbar>
+    );
+}
